c57f6a86a8faf9f0583ddf595beaf6ecda658f17
Ilhamalami <ilhama19@gmail.com>
2020-04-27 12:20:14 +0300

    Update detection_home.dart

--- lib/detection_home.dart
+++ lib/detection_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'face_detection.dart';
+import 'detection.dart';
 
 class FaceDetectorHome extends StatefulWidget {
   FaceDetectorHome({Key key}) : super(key: key);
